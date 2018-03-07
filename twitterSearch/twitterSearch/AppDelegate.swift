6b8e9f4b4c7a40e91b238768ebe3c588bc0977fd
Kajornsak Peerapathananont <kajornsak_psm@hotmail.com>
2018-03-08 01:55:49 +0700

    [+] added vc in storyboard, link search wireframe to app

--- twitterSearch/twitterSearch/AppDelegate.swift
+++ twitterSearch/twitterSearch/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -16,6 +17,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let searchView = SearchWireframe.createSearchModule()
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = searchView
+        window?.makeKeyAndVisible()
         return true
     }
 
@@ -40,7 +45,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
 
 }
 
